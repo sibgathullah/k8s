350e5fc9b6552b63d430642112993b46f7091245
sibgathullah <87558867+sibgathullah@users.noreply.github.com>
2021-08-25 06:44:31 +0530

    Create worker.sh

---
+++ worker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install  -y default-jre
+sudo apt-get install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+sudo apt update -y
+sudo apt-get install kubeadm kubelet kubectl -y
+sudo swapoff -a
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ]
+}
+EOF
+mkdir -p /etc/systemd/system/docker.service.d
+
+systemctl daemon-reload
+systemctl restart docker
